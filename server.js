f716a89cdb7188cac5481a64731f1237a884b0b3
kenxsantos <133564965+kenxsantos@users.noreply.github.com>
2024-11-27 15:11:35 +0800

    store data to firebase

--- server.js
+++ server.js
@@ -18,17 +18,22 @@ const io = socketIo(server, {
 
 // Initialize Firebase Admin SDK
 const serviceAccount = require("./firebase-service-account.json"); // Replace with your Firebase service account key
-const { timeStamp } = require("console");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://swiftpath-77a56-default-rtdb.firebaseio.com/",
 });
+
+const db = admin.database();
+
+// Your Roam.ai API key
+const ROAM_API_KEY = process.env.ROAM_API_KEY;
 
 let currentCoordinates = null;
 let destinationCoordinates = null;
 let vehicleLocation = {};
 
 // Parse incoming JSON
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "*", methods: ["GET", "POST"] }));
 app.use(bodyParser.json());
 
 app.post("/report-incident", async (req, res) => {
@@ -38,32 +43,38 @@ app.post("/report-incident", async (req, res) => {
     latitude,
     longitude,
     status,
+    address,
+    details,
     reporter_email,
     reporter_name,
     timestamp,
     image_url,
   } = req.body;
 
-  console.log(
-    latitude,
-    longitude,
-    status,
-    reporter_email,
-    reporter_name,
-    timestamp,
-    image_url
-  );
-  // const { userId, location } = req.body;
-  // vehicleLocation = {
-  //   latitude: location.latitude,
-  //   longitude: location.longitude,
-  // };
+  try {
+    const geofenceId = await createGeofence(latitude, longitude);
+    if (geofenceId) {
+      await storeIncidentReport(
+        latitude,
+        longitude,
+        status,
+        reporter_email,
+        reporter_name,
+        timestamp,
+        image_url,
+        geofenceId,
+        address,
+        details
+      );
 
-  // console.log(
-  //   `Updated location: ${vehicleLocation.latitude}, ${vehicleLocation.longitude}`
-  // );
-  // io.emit("locationUpdate", vehicleLocation);
-  // res.status(200).send("Location updated");
+      res.status(200).json({ message: "Incident reported successfully." });
+    } else {
+      res.status(500).json({ message: "Failed to create geofence." });
+    }
+  } catch (error) {
+    console.error("Error handling incident report:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
 });
 
 app.post("/current-location", async (req, res) => {
@@ -87,11 +98,152 @@ app.post("/current-location", async (req, res) => {
       .send(
         "Location received successfully. Calculating routes to destination..."
       );
-    // io.emit("location_update", { coordinates: currentCoordinates }); // Emit the updated coordinates
     await requestRoutes();
   } catch (error) {
     console.error("Error handling destination:", error);
     res.status(500).send("Error processing destination");
+  }
+});
+
+async function createGeofence(latitude, longitude) {
+  try {
+    const now = new Date();
+    const endTime = new Date(now.getTime() + 24 * 60 * 60 * 1000); // 1 day later
+    const geofenceData = {
+      coordinates: [longitude, latitude],
+      geometry_radius: 500,
+      description: "Incident Location",
+      tag: "Incident Report",
+      metadata: {},
+      is_enabled: [true, now.toISOString(), endTime.toISOString()],
+    };
+
+    const response = await axios.post(
+      "https://api.roam.ai/v1/api/geofence/",
+      geofenceData,
+      {
+        headers: {
+          "Api-Key": ROAM_API_KEY,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (response.status === 201) {
+      const geofenceId = response.data.data.geofence_id;
+      console.log("Geofence created successfully:", geofenceId);
+      return geofenceId;
+    } else {
+      console.error("Failed to create geofence:", response.data);
+      return null;
+    }
+  } catch (error) {
+    console.error("Error creating geofence:", error);
+    return null;
+  }
+}
+
+async function storeIncidentReport(
+  latitude,
+  longitude,
+  status,
+  reporter_email,
+  reporter_name,
+  timestamp,
+  image_url,
+  geofenceId,
+  address,
+  details
+) {
+  try {
+    const incidentData = {
+      geofence_id: geofenceId,
+      image_url: image_url,
+      latitude: latitude,
+      longitude: longitude,
+      address: address,
+      details: details,
+      reporter_name: reporter_name,
+      reporter_email: reporter_email,
+      status: status,
+      timestamp: timestamp,
+    };
+
+    await db.ref(`incident-reports/`).push(incidentData);
+    console.log("Incident stored successfully.");
+  } catch (error) {
+    console.error("Error storing incident:", error);
+    throw error;
+  }
+}
+
+app.post("/emergency-vehicle-location", async (req, res) => {
+  try {
+    const { origin, userId, is_tracking } = req.body;
+
+    // Check if the required parameters are provided
+    if (!userId || !origin) {
+      return res.status(400).send("Invalid destination payload");
+    }
+
+    console.log(
+      "Received location for userId:",
+      userId,
+      "Location:",
+      origin,
+      "is_tracking:",
+      is_tracking
+    );
+
+    const ref = db.ref("emergency-vehicle-location");
+
+    // First, check if the userId already exists in the database
+    ref.child(userId).once("value", (snapshot) => {
+      if (snapshot.exists()) {
+        // User exists, update their location
+        ref.child(userId).update(
+          {
+            origin: origin,
+            is_tracking: is_tracking,
+          },
+          (error) => {
+            if (error) {
+              console.log("Error updating data in Firebase:", error);
+              res.status(500).send("Error updating location");
+            } else {
+              console.log("Location updated successfully for userId:", userId);
+              res.status(200).send("Location updated successfully");
+            }
+          }
+        );
+      } else {
+        // User does not exist, create a new entry
+        const emergencyVehicleData = {
+          [userId]: {
+            origin: origin,
+            is_tracking: is_tracking,
+            userId: userId,
+          },
+        };
+
+        // Set the new user data
+        ref.update(emergencyVehicleData, (error) => {
+          if (error) {
+            console.log("Error writing data to Firebase:", error);
+            res.status(500).send("Error adding new location");
+          } else {
+            console.log(
+              "New emergency vehicle data added successfully for userId:",
+              userId
+            );
+            res.status(200).send("New location added successfully");
+          }
+        });
+      }
+    });
+  } catch (error) {
+    console.error("Error handling location:", error);
+    res.status(500).send("Error processing location");
   }
 });
 
@@ -150,7 +302,6 @@ async function requestRoutes() {
 app.post("/webhook", async (req, res) => {
   try {
     const events = req.body.events || [req.body]; // Handle both wrapped events and single event
-
     console.log("Received events:", events);
 
     if (!events || (!Array.isArray(events) && !events.event_type)) {
